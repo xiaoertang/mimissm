854d8fe24a0a298ced4057d31f1c84348285e95c
xiaoertang <1298840110@qq.com>
2022-01-08 17:07:30 +0800

    使用监听器ServletContextListener，实现全局作用域，供全部商品类别下拉集合

--- src/main/java/com/yurora/listener/ProductTypeListener.java
+++ src/main/java/com/yurora/listener/ProductTypeListener.java
@@ -20,7 +20,7 @@ public class ProductTypeListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         //手工从Spring容器中获取ProductTypeServiceImpl的对象
-        ApplicationContext context = new ClassPathXmlApplicationContext("application_*.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_*.xml");
         ProductTypeService productTypeService = (ProductTypeService) context.getBean("ProductTypeServiceImpl");
         List<ProductType> typeList = productTypeService.getAll();
         //放在全局应用作用域中，供新增页面，修改页面，前台的查询功能提供全部商品类别集合
